919934a7cc6b21c6bd98b7e27373333f4b1d50c3
andrewzic <andrew.john.zic@gmail.com>
2025-08-21 14:59:29 +1000

    add test_vis_ffa_image to __init__

--- src/riptide/__init__.py
+++ src/riptide/__init__.py
@@ -10,7 +10,7 @@ from .candidate import Candidate
 
 ### Major functions
 from .search import ffa_search
-from .vis_search import vis_ffa_search, vis_ffa_search_basep, vis_ffa_image_candidates
+from .vis_search import vis_ffa_search, vis_ffa_search_basep, vis_ffa_image_candidates, test_vis_ffa_image
 from .running_medians import running_median, fast_running_median
 from .ffautils import plan_ffa
 
@@ -33,6 +33,7 @@ __all__ = [
     "ffa_search",
     "vis_ffa_search",
     "vis_ffa_search_basep",
+    "test_vis_ffa_image",
     "vis_ffa_image_candidates",
     "ffa1",
     "ffa2",
