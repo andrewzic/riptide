3363bfb95cab777b7a2410ac822d24496c70b739
andrewzic <andrew.john.zic@gmail.com>
2025-09-01 21:20:33 +1000

    plan_tau bugfix

--- src/riptide/pipeline/worker_pool.py
+++ src/riptide/pipeline/worker_pool.py
@@ -656,7 +656,7 @@ class VisWorkerPool(object):
                 bins = ffa_plan["bins"]
                 rows_eval = ffa_plan["rows_eval"]
 
-                periods = libffa.ffaprd(N=nsamp, p=int(bins), dt=plan_tau)
+                periods = libffa.ffaprd(N=nsamp, p=int(bins), dt=tau)
                 # Step 1: run vis_ffa_search for each UV cell
 
                 print(f"doing FFA transform on {vis_ts.unique_uv.shape} cells with base period {base_period}")
